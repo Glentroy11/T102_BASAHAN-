8306894373084420e4baea1216406d7c231374fe
zaneeey <91510153+Glentroy11@users.noreply.github.com>
2021-11-19 12:02:46 +0800

    Update ma3finaltask.cpp

--- ma3finaltask.cpp
+++ ma3finaltask.cpp
@@ -1,71 +1,57 @@
 #include<stdio.h>
 int main(){
 int choices, price, payment, change;
-    printf("Please pick you favorite order");
-    printf("\n[1]Coca-cola (18.00) \n[2] Super crunch (10.00) \n[3] Royal (18.00) [4]hansel (8.00)\nChoice:");
+    printf("Select your order");
+    printf("\n[1]Pepsi(P100) \n[2]Dr.Pepper(P120) \n[3]Coca-cola(P95)\nChoice:");
     scanf("%d", &choices);
     switch (choices){
     
     case 1:
-    price = 18;
-    printf("Please Input payment:");
+    price = 100;
+    printf("Input payment:");
     scanf("%d", &payment);
     if(payment>=price){
-    printf("Success! Enjoy your Pepsi Sir/Ma'am.\n");
+    printf("You have purchase Pepsi.\n");
     change = payment-price;  
     printf("\nChange: %d\n", change);  
 }
 else{ 
-    printf("Not enough Funds");
+    printf("You dont have enough Funds.");
 }
     break;
     
     case 2:
-    price = 10;
-    printf("Please Input payment:");
+    price = 120;
+    printf("Input payment:");
     scanf("%d", &payment);
     if(payment>=price){
-    printf("Succes! Enjoy your DrPepper Sir/Ma'am.\n");
+    printf("You have purchase Dr.Pepper.\n");
     change = payment-price;  
     printf("\nChange: %d\n", change);
 }
 else{ 
 
-    printf("Not enough Funds");
+    printf("You dont have enough Funds.");
 }
      break;
    
     
-     case 3:
-    price = 18;
-    printf("Please Input payment:");
+
+  case 3:
+    price = 95;
+    printf("Input payment:");
     scanf("%d", &payment);
     if(payment>=price){
-    printf("Success! Enjoy your Cocacola Sir/Ma'am.\n");
+    printf("You have purchase Coca-cola.\n");
     change = payment-price;  
     printf("\nChange: %d\n", change);
 }
 else {
-  printf("Not enough Funds");
-}
-  break;
-  
-  
-  case 4:
-    price = 8;
-    printf("Please Input payment:");
-    scanf("%d", &payment);
-    if(payment>=price){
-    printf("Success! Enjoy your Cocacola Sir/Ma'am.\n");
-    change = payment-price;  
-    printf("\nChange: %d\n", change);
-}
-else {
-  printf("Not enough Funds");
+  printf("You dont have enough Funds.");
 }
   break;
    default:
-   printf("Purchase unavailable");
+   printf("Invalid Input");
 }
 return 0;
 }
